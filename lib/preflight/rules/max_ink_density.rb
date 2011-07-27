efddd7b0cfbac525a347acfa6f77d0da9a2e3a55
James Healy <jimmy@deefa.com>
2011-07-27 15:14:22 +1000

    new rule: maximum CMYK ink density

---
+++ lib/preflight/rules/max_ink_density.rb
@@ -0,0 +1,65 @@
+# coding: utf-8
+
+require 'yaml'
+require 'matrix'
+
+module Preflight
+  module Rules
+
+    # Most CMYK printers will have a stated upper tolerance for ink density. If
+    # the total percentage of the 4 components (C, M, Y and K) is over that 
+    # tolerance then the result can be unpredictable and often ugly.
+    #
+    # Use this rule to detect CMYK ink densities over a certain threshold.
+    #
+    # Arguments: the highest density that is Ok
+    #
+    # Usage:
+    #
+    #   class MyPreflight
+    #     include Preflight::Profile
+    #
+    #     rule Preflight::Rules::MaxInkDensity, 300
+    #   end
+    #
+    # TODO:
+    #
+    # * check CMYK colours used as alternates in a separation color
+    # * check CMYK raster images
+    #
+    class MaxInkDensity
+
+      attr_reader :messages
+
+      def initialize(max_ink)
+        @max_ink = max_ink.to_i
+      end
+
+      # we're about to start a new page, reset state
+      #
+      def page=(page)
+        @messages = []
+        @page    = page
+        @objects = page.objects
+      end
+
+      def set_cmyk_color_for_nonstroking(c, m, y, k)
+        check_ink(c, m, y, k)
+      end
+
+      def set_cmyk_color_for_stroking(c, m, y, k)
+        check_ink(c, m, y, k)
+      end
+
+      private
+
+      def check_ink(c, m, y, k)
+        ink = (c + m + y + k) * 100.0
+        if ink > @max_ink && @messages.empty?
+          @messages << "Ink density on page #{@page.number} too high (c: #{c}, m: #{m}, y: #{y}, k: #{k})"
+        end
+      end
+    end
+
+  end
+end
---
+++ spec/rules/max_ink_density_spec.rb
@@ -0,0 +1,36 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe Preflight::Rules::MaxInkDensity do
+
+  it "pass files with a no CYMK colors" do
+    filename = pdf_spec_file("rgb-hexagon")
+    rule     = Preflight::Rules::MaxInkDensity.new(100)
+
+    PDF::Reader.open(filename) do |reader|
+      reader.page(1).walk(rule)
+      rule.messages.should be_empty
+    end
+  end
+
+  it "pass files with a CYMK color that has a suitable ink density" do
+    filename = pdf_spec_file("pdfx-1a-no-subsetting")
+    rule     = Preflight::Rules::MaxInkDensity.new(300)
+
+    PDF::Reader.open(filename) do |reader|
+      reader.page(1).walk(rule)
+      rule.messages.should be_empty
+    end
+  end
+
+  it "fail files with a CYMK color that has a high ink density" do
+    filename = pdf_spec_file("pdfx-1a-no-subsetting")
+    rule     = Preflight::Rules::MaxInkDensity.new(50)
+
+    PDF::Reader.open(filename) do |reader|
+      reader.page(1).walk(rule)
+      rule.messages.should_not be_empty
+    end
+  end
+
+
+end
