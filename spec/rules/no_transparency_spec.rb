ff10ff747358875d62aaac0d985d21ce07b33fc5
James Healy <jimmy@deefa.com>
2011-08-02 20:10:14 +1000

    add pendign spec for NoTransparency rule

---
+++ spec/rules/no_transparency_spec.rb
@@ -0,0 +1,18 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe Preflight::Rules::NoTransparency do
+
+  # TODO find sample PDFs with transparency that aren't commercially sensitive
+  it "should fail files that use transparency"
+
+  it "should pass files that are vlaid PDF/X-1a" do
+    filename = pdf_spec_file("pdfx-1a-no-subsetting")
+    rule     = Preflight::Rules::NoTransparency.new
+
+    PDF::Reader.open(filename) do |reader|
+      reader.page(1).walk(rule)
+      rule.messages.should be_empty
+    end
+  end
+
+end
