d2a285dbd4f2b9f339a4c9602f7302f313a08d59
James Healy <jimmy@deefa.com>
2011-08-02 23:01:58 +1000

    spec to test for the bug fixed in c78e93

--- spec/profiles/custom_profile_spec.rb
+++ spec/profiles/custom_profile_spec.rb
@@ -8,6 +8,12 @@ class CustomProfile
   rule Preflight::Rules::MaxVersion, 1.3
 end
 
+class PpiProfile
+  include Preflight::Profile
+
+  rule Preflight::Rules::MinPpi, 200
+end
+
 describe "Customised profile" do
 
   it "fail files with a higher version" do
@@ -21,6 +27,22 @@ describe "Customised profile" do
   it "pass files with an equal version" do
     filename  = pdf_spec_file("version_1_3")
     preflight = CustomProfile.new
+    messages  = preflight.check(filename)
+
+    messages.should be_empty
+  end
+
+  it "fail files with a low ppi" do
+    filename = pdf_spec_file("72ppi")
+    preflight = PpiProfile.new
+    messages  = preflight.check(filename)
+
+    messages.should_not be_empty
+  end
+
+  it "pass files with high ppi" do
+    filename  = pdf_spec_file("300ppi")
+    preflight = PpiProfile.new
     messages  = preflight.check(filename)
 
     messages.should be_empty
