1b033b6c06d06cd63ac4b415749cef038aed7c4e
James Healy <jimmy@deefa.com>
2011-08-03 15:29:10 +1000

    Revert "allow filespecs if they refer to an embedded file"
    This reverts commit 6b44d21b4bbb0e0c147a92b10fdb6bfc3201d0a4.

--- lib/preflight/rules/no_filespecs.rb
+++ lib/preflight/rules/no_filespecs.rb
@@ -18,7 +18,7 @@ module Preflight
     class NoFilespecs
 
       def check_hash(ohash)
-        if has_filespecs(ohash)
+        if count_filespec_dicts(ohash) > 0
           ["File uses at least 1 Filespec to refer to an external file"]
         else
           []
@@ -27,10 +27,10 @@ module Preflight
 
       private
 
-      def has_filespecs(ohash)
-        ohash.any? { |key, obj|
-          obj.is_a?(::Hash) && (obj[:Type] == :Filespec || obj[:Type] == :F) && obj[:EF].nil?
-        }
+      def count_filespec_dicts(ohash)
+        ohash.select { |key, obj|
+          obj.is_a?(::Hash) && (obj[:Type] == :Filespec || obj[:Type] == :F)
+        }.size
       end
     end
   end
