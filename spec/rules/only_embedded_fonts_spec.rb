c640644bd326a97b4544f1825d2d02d34ed1904c
James Healy <jimmy@deefa.com>
2011-08-12 13:25:15 +1000

    spec embedded fonts rule

---
+++ spec/rules/only_embedded_fonts_spec.rb
@@ -0,0 +1,38 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe Preflight::Rules::OnlyEmbeddedFonts do
+
+  it "should pass a PDF/X-1a file with a no subsetting" do
+    filename = pdf_spec_file("pdfx-1a-no-subsetting")
+    rule     = Preflight::Rules::OnlyEmbeddedFonts.new
+
+    PDF::Reader.open(filename) do |reader|
+      reader.page(1).walk(rule)
+      rule.messages.should be_empty
+    end
+  end
+
+  it "should pass a PDF/X-1a file that uses subsetting" do
+    filename = pdf_spec_file("pdfx-1a-subsetting")
+    rule     = Preflight::Rules::OnlyEmbeddedFonts.new
+
+    PDF::Reader.open(filename) do |reader|
+      reader.page(1).walk(rule)
+      rule.messages.should be_empty
+    end
+  end
+
+  it "should pass a file that only uses Type 3 fonts"
+
+  it "should pass a file that has no text"
+
+  it "fail a file that uses one of the base-14 fonts without embedding" do
+    filename = pdf_spec_file("standard_14_font")
+    rule     = Preflight::Rules::OnlyEmbeddedFonts.new
+
+    PDF::Reader.open(filename) do |reader|
+      reader.page(1).walk(rule)
+      rule.messages.should_not be_empty
+    end
+  end
+end
