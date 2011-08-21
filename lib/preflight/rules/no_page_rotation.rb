8bebb695412168ab34a1756ea912dee9af52aca7
James Healy <jimmy@deefa.com>
2011-08-21 12:52:49 +1000

    new rule: check for page rotation

---
+++ lib/preflight/rules/no_page_rotation.rb
@@ -0,0 +1,36 @@
+# coding: utf-8
+
+module Preflight
+  module Rules
+
+    # Ensure the target file contains no page rotation.
+    #
+    # Rotating pages is generally acceptable in most specs but it can cause
+    # rendering issues with poor waulity PDF consumers.
+    #
+    # Arguments: none
+    #
+    # Usage:
+    #
+    #   class MyPreflight
+    #     include Preflight::Profile
+    #
+    #     rule Preflight::Rules::NoPageRotation
+    #   end
+    #
+    class NoPageRotation
+
+      attr_reader :messages
+
+      def page=(page)
+        attrs = page.attributes
+
+        if attrs[:Rotate]
+          @messages = ["Page is rotated (page #{page.number})"]
+        else
+          @messages = []
+        end
+      end
+    end
+  end
+end
---
+++ spec/rules/no_page_rotation_spec.rb
@@ -0,0 +1,25 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe Preflight::Rules::NoPageRotation do
+
+  it "pass files with no page Rotate entry" do
+    filename = pdf_spec_file("pdfx-1a-subsetting")
+    rule     = Preflight::Rules::NoPageRotation.new
+
+    PDF::Reader.open(filename) do |reader|
+      reader.page(1).walk(rule)
+      rule.messages.should be_empty
+    end
+  end
+
+  it "fail files with a page Rotate entry" do
+    filename = pdf_spec_file("rotate")
+    rule     = Preflight::Rules::NoPageRotation.new
+
+    PDF::Reader.open(filename) do |reader|
+      reader.page(1).walk(rule)
+      rule.messages.should_not be_empty
+    end
+  end
+
+end
