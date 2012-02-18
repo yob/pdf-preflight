b25a85408ce1f12af3789eae263fdd8777976b3d
James Healy <jimmy@deefa.com>
2012-02-18 16:50:44 +1100

    MinBleed rule should delegate to PageState for state logic

--- lib/preflight/rules/min_bleed.rb
+++ lib/preflight/rules/min_bleed.rb
@@ -26,12 +26,15 @@ module Preflight
     #
     class MinBleed
       include Preflight::Measurements
+      extend  Forwardable
 
       attr_reader :issues
 
-      DEFAULT_GRAPHICS_STATE = {
-        :ctm => Matrix.identity(3)
-      }
+      # Graphics State Operators
+      def_delegators :@state, :save_graphics_state, :restore_graphics_state
+
+      # Matrix Operators
+      def_delegators :@state, :concatenate_matrix
 
       def initialize(range, bleed, units)
         @range, @bleed, @units = range, bleed, units
@@ -40,12 +43,10 @@ module Preflight
       # we're about to start a new page, reset state
       #
       def page=(page)
-        @issues = []
+        @issues  = []
         @page    = page
+        @state   = PDF::Reader::PageState.new(page)
         @objects = page.objects
-        @stack   = [DEFAULT_GRAPHICS_STATE]
-        @xobjects = @page.xobjects || {}
-        @form_xobjects = {}
 
         attrs = @page.attributes
         box   = attrs[:TrimBox] || attrs[:ArtBox] || attrs[:MediaBox]
@@ -59,60 +60,28 @@ module Preflight
         @error_max_y   = box[3] + to_points(@bleed, @units)
       end
 
-      def save_graphics_state
-        @stack.push clone_state
-      end
-
-      def restore_graphics_state
-        @stack.pop
-      end
-
-      # update the current transformation matrix.
-      #
-      # If the CTM is currently undefined, just store the new values.
-      #
-      # If there's an existing CTM, then multiple the existing matrix
-      # with the new matrix to form the updated matrix.
-      #
-      def concatenate_matrix(*args)
-        transform = Matrix[
-          [args[0], args[1], 0],
-          [args[2], args[3], 0],
-          [args[4], args[5], 1]
-        ]
-        if state[:ctm]
-          state[:ctm] = transform * state[:ctm]
-        else
-          state[:ctm] = transform
-        end
-      end
-
       # As each image is drawn on the canvas, determine the amount of device
       # space it's being crammed into and therefore the PPI.
       #
       def invoke_xobject(label)
-        save_graphics_state
-        xobject = @objects.deref(xobject(label))
-
-        matrix = xobject.hash[:Matrix]
-        concatenate_matrix(*matrix) if matrix
-
-        case xobject.hash[:Subtype]
-        when :Form  then invoke_form_xobject(label)
-        when :Image then invoke_image_xobject(label)
-        else
-          # ignore other xobject types for now
+        @state.invoke_xobject(label) do |xobj|
+          case xobj
+          when PDF::Reader::FormXObject then
+            xobj.walk(self)
+          when PDF::Reader::Stream
+            invoke_image_xobject(xobj) if xobj.hash[:Subtype] == :Image
+          else
+            raise xobj.inspect
+          end
         end
-
-        restore_graphics_state
       end
 
       def append_rectangle(x1, y1, x2, y2)
         @path ||= []
-        @path << transform(Point.new(x1, y1))
-        @path << transform(Point.new(x1, y2))
-        @path << transform(Point.new(x2, y1))
-        @path << transform(Point.new(x2, y2))
+        @path << @state.ctm_transform(x1, y1)
+        @path << @state.ctm_transform(x1, y2)
+        @path << @state.ctm_transform(x2, y1)
+        @path << @state.ctm_transform(x2, y2)
       end
 
       def fill_path_with_nonzero
@@ -144,21 +113,7 @@ module Preflight
 
       private
 
-      def xobject(label)
-        deref(@form_xobjects[label] || @xobjects[label])
-      end
-
-      def invoke_form_xobject(label)
-        return unless xobject(label)
-        xobject = @objects.deref(xobject(label))
-        form = PDF::Reader::FormXObject.new(@page, xobject)
-        @form_xobjects = (form.resources || {})[:XObject] || {}
-        form.walk(self)
-        @form_xobjects = {}
-      end
-
-      def invoke_image_xobject(label)
-        return unless xobject(label)
+      def invoke_image_xobject(xobject)
         return unless @page.attributes[:TrimBox] || @page.attributes[:ArtBox]
 
         points = select_points_in_danger_zone(image_points)
@@ -173,26 +128,6 @@ module Preflight
 
       def deref(obj)
         @objects ? @objects.deref(obj) : obj
-      end
-
-      # return the current transformation matrix
-      #
-      def ctm
-        state[:ctm]
-      end
-
-      def state
-        @stack.last
-      end
-
-      # transform x and y co-ordinates from the current user space to the
-      # underlying device space.
-      #
-      def transform(point, z = 1)
-        Point.new(
-          (ctm[0,0] * point.x) + (ctm[1,0] * point.y) + (ctm[2,0] * z),
-          (ctm[0,1] * point.x) + (ctm[1,1] * point.y) + (ctm[2,1] * z)
-        )
       end
 
       # convert value units to PDF points. units should be :mm, :in or :pt
@@ -211,54 +146,24 @@ module Preflight
       #
       def image_points
         [
-          transform(Point.new(0, 0)),
-          transform(Point.new(0, 1)),
-          transform(Point.new(1, 0)),
-          transform(Point.new(1, 1))
+          @state.ctm_transform(0, 0),
+          @state.ctm_transform(0, 1),
+          @state.ctm_transform(1, 0),
+          @state.ctm_transform(1, 1)
         ]
       end
 
       # given an array of points, returns the subset of points (if any) that
-      # fall within the dnager zone indicating they're close to the TrimBox
+      # fall within the danger zone indicating they're close to the TrimBox
       # without enough bleed
       #
       def select_points_in_danger_zone(points)
         points.select { |p|
-          (p.x < @warning_min_x && p.x > @error_min_x) ||
-          (p.y < @warning_min_y && p.y > @error_min_y) ||
-          (p.x > @warning_max_x && p.x < @error_max_x) ||
-          (p.y > @warning_max_x && p.y < @error_max_y)
+          (p.first < @warning_min_x && p.first > @error_min_x) ||
+          (p.last < @warning_min_y  && p.last > @error_min_y) ||
+          (p.first > @warning_max_x && p.first < @error_max_x) ||
+          (p.last > @warning_max_x  && p.last < @error_max_y)
         }
-      end
-
-      # when save_graphics_state is called, we need to push a new copy of the
-      # current state onto the stack. That way any modifications to the state
-      # will be undone once restore_graphics_state is called.
-      #
-      # This returns a deep clone of the current state, ensuring changes are
-      # keep separate from earlier states.
-      #
-      # YAML is used to round-trip the state through a string to easily perform
-      # the deep clone. Kinda hacky, but effective.
-      #
-      def clone_state
-        if @stack.empty?
-          {}
-        else
-          yaml_state = YAML.dump(@stack.last)
-          YAML.load(yaml_state)
-        end
-      end
-    end
-
-    # private class for representing points on a cartesian plain. Used
-    # to simplify maths in the MinBleed class.
-    #
-    class Point
-      attr_reader :x, :y
-
-      def initialize(x,y)
-        @x, @y = x,y
       end
 
     end
