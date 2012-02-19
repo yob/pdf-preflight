304138e84a9035ca7a575dfe7020eba9e656130c
James Healy <jimmy@deefa.com>
2012-02-19 17:47:24 +1100

    update the NoTransparency rule to include co-ordinates on the Issue

--- lib/preflight/rules/no_transparency.rb
+++ lib/preflight/rules/no_transparency.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-require 'yaml'
+require 'forwardable'
 
 module Preflight
   module Rules
@@ -19,32 +19,66 @@ module Preflight
     #
     class NoTransparency
       include Preflight::Measurements
+      extend  Forwardable
+
+      # Graphics State Operators
+      def_delegators :@state, :save_graphics_state, :restore_graphics_state
+
+      # Matrix Operators
+      def_delegators :@state, :concatenate_matrix
 
       attr_reader :issues
 
       # we're about to start a new page, reset state
       #
       def page=(page)
-        @issues   = []
-        @page     = page
-        @objects  = page.objects
-        @xobjects = @page.xobjects
+        @page   = page
+        @state  = PDF::Reader::PageState.new(page)
+        @issues = []
+      end
+
+      def invoke_xobject(label)
+        @state.invoke_xobject(label) do |xobj|
+          case xobj
+          when PDF::Reader::FormXObject then
+            detect_transparent_form(xobj)
+            xobj.walk(self)
+          else
+            # TODO. can other xobjects have transparency?
+          end
+        end
       end
 
       # As each xobject is drawn on the canvas, record if it's Group XObject
       # with transparency
       #
-      def invoke_xobject(label)
-        xobj  = deref(@xobjects[label])
-        group = deref(xobj.hash[:Group]) if xobj
-        stype = deref(group[:S])         if group
+      def detect_transparent_form(form)
+        xobject = form.xobject
+        group   = deref(xobject.hash[:Group])
+        stype   = deref(group[:S])            if group
 
         if stype == :Transparency
-          @issues << Issue.new("Transparent xobject found", self, :page => @page.number)
+          bbox = xobject.hash[:BBox] || @page.attributes[:MediaBox]
+          bbox = translate_to_device_space(bbox)
+          @issues << Issue.new("Transparent xobject found", self, :page         => @page.number,
+                                                                  :top_left     => bbox[:tl],
+                                                                  :bottom_left  => bbox[:bl],
+                                                                  :bottom_right => bbox[:br],
+                                                                  :top_right    => bbox[:tr])
         end
       end
 
       private
+
+      def translate_to_device_space(bbox)
+        bl_x, bl_y, tr_x, tr_y = *bbox
+        {
+          :tl => @state.ctm_transform(bl_x, tr_y),
+          :bl => @state.ctm_transform(bl_x, bl_y),
+          :br => @state.ctm_transform(tr_x, bl_y),
+          :tr => @state.ctm_transform(tr_x, tr_y)
+        }
+      end
 
       def deref(obj)
         @objects ? @objects.deref(obj) : obj
--- spec/rules/no_transparency_spec.rb
+++ spec/rules/no_transparency_spec.rb
@@ -2,26 +2,38 @@ require File.dirname(__FILE__) + "/../spec_helper"
 
 describe Preflight::Rules::NoTransparency do
 
-  it "should fail files that use transparency" do
-    filename = pdf_spec_file("transparency") # page 2 has transparency
-    rule     = Preflight::Rules::NoTransparency.new
+  context "a page with transparent xobjects" do
+    let!(:filename) { pdf_spec_file("transparency") }
 
-    PDF::Reader.open(filename) do |reader|
-      reader.page(2).walk(rule)
-      rule.issues.should have(1).item
-      issue = rule.issues.first
-      issue.rule.should == :"Preflight::Rules::NoTransparency"
-      issue.attributes[:page].should == 2
+    it "should fail with an issue" do
+      rule     = Preflight::Rules::NoTransparency.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(2).walk(rule)
+
+        rule.issues.should have(1).item
+
+        issue = rule.issues.first
+        issue.rule.should         == :"Preflight::Rules::NoTransparency"
+        issue.page.should         == 2
+        issue.top_left.should     == [99.0, 540.89]
+        issue.bottom_left.should  == [99.0, 742.89]
+        issue.bottom_right.should == [301.0, 742.89]
+        issue.top_right.should    == [301.0, 540.89]
+      end
     end
   end
 
-  it "should pass files that have no transparency" do
-    filename = pdf_spec_file("transparency") # page 1 has no transparency
-    rule     = Preflight::Rules::NoTransparency.new
+  context "a page without transparent xobjects" do
+    let!(:filename) { pdf_spec_file("transparency") }
 
-    PDF::Reader.open(filename) do |reader|
-      reader.page(1).walk(rule)
-      rule.issues.should be_empty
+    it "should pass with no issues" do
+      rule     = Preflight::Rules::NoTransparency.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(1).walk(rule)
+        rule.issues.should be_empty
+      end
     end
   end
 
