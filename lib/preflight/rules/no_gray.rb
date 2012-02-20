2018c3cc0d0adb8302f79a74862fa8e8ae9207a0
James Healy <jimmy@deefa.com>
2012-02-20 19:02:07 +1100

    new rule: NoGray
    * detect files that include gray scale color
    * this is probably not going to be used much, but in the interests of
      completeness I may as well include it

---
+++ lib/preflight/rules/no_gray.rb
@@ -0,0 +1,105 @@
+# coding: utf-8
+
+require 'forwardable'
+
+module Preflight
+  module Rules
+
+    # Some print workflows forbid the use of Gray colour.
+    #
+    # Arguments: none
+    #
+    # Usage:
+    #
+    #   class MyPreflight
+    #     include Preflight::Profile
+    #
+    #     rule Preflight::Rules::NoGray
+    #   end
+    #
+    class NoGray
+      extend  Forwardable
+
+      # Graphics State Operators
+      def_delegators :@state, :save_graphics_state, :restore_graphics_state
+
+      # Matrix Operators
+      def_delegators :@state, :concatenate_matrix
+
+      attr_reader :issues
+
+      # we're about to start a new page, reset state
+      #
+      def page=(page)
+        @page   = page
+        @state  = PDF::Reader::PageState.new(page)
+        @issues = []
+        @resource_labels_seen = []
+      end
+
+      # descend into nested form xobjects
+      #
+      def invoke_xobject(label)
+        @state.invoke_xobject(label) do |xobj|
+          case xobj
+          when PDF::Reader::FormXObject then
+            xobj.walk(self)
+          when PDF::Reader::Stream then
+            check_xobject(xobj)
+          end
+        end
+      end
+
+      def set_gray_for_stroking(g)
+        gray_detected(g)
+      end
+
+      def set_gray_for_nonstroking(g)
+        gray_detected(g)
+      end
+
+      def set_stroke_color_space(label)
+        check_color_space(label)
+      end
+
+      def set_nonstroke_color_space(label)
+        check_color_space(label)
+      end
+
+      private
+
+      def color_space_is_gray?(cs)
+        case cs
+        when Symbol then cs == :DeviceGray
+        when Array  then
+          cs[0] == DeviceGray || cs[2] == :DeviceGray
+        else
+          false
+        end
+      end
+
+      def check_color_space(label)
+        return if @resource_labels_seen.include?(label)
+
+        if color_space_is_gray?(@state.find_color_space(label))
+          @issues << Issue.new("Gray color detected", self, :page  => @page.number)
+        end
+
+        @resource_labels_seen << label
+      end
+
+      def check_xobject(xobject)
+        cs = xobject.hash[:ColorSpace]
+        if cs == :DeviceGray
+          @issues << Issue.new("Gray image detected", self, :page  => @page.number)
+        end
+      end
+
+      def gray_detected(g)
+        @issues << Issue.new("Gray color detected", self, :page  => @page.number,
+                                                          :gray  => g)
+      end
+    end
+
+  end
+end
---
+++ spec/rules/no_gray_spec.rb
@@ -0,0 +1,75 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe Preflight::Rules::NoGray do
+
+  context "a page with a Gray fill colour" do
+    let!(:filename) { pdf_spec_file("gray") }
+
+    it "should have one an issue" do
+      rule     = Preflight::Rules::NoGray.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(1).walk(rule)
+      end
+
+      rule.issues.should have(1).item
+    end
+  end
+
+  context "a page with a Gray stroke colour" do
+    let!(:filename) { pdf_spec_file("gray") }
+
+    it "should have one an issue" do
+      rule     = Preflight::Rules::NoGray.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(2).walk(rule)
+      end
+
+      rule.issues.should have(1).item
+    end
+  end
+
+  context "a page with a Gray fill colour defined in the page resources" do
+    let!(:filename) { pdf_spec_file("gray") }
+
+    it "should have one an issue" do
+      rule     = Preflight::Rules::NoGray.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(3).walk(rule)
+      end
+
+      rule.issues.should have(1).item
+    end
+  end
+
+  context "a page with a Gray stroke colour defined in the page resources" do
+    let!(:filename) { pdf_spec_file("gray") }
+
+    it "should have one an issue" do
+      rule     = Preflight::Rules::NoGray.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(4).walk(rule)
+      end
+
+      rule.issues.should have(1).item
+    end
+  end
+
+  context "a page with a Gray image" do
+    let!(:filename) { pdf_spec_file("gray") }
+
+    it "should have one an issue" do
+      rule     = Preflight::Rules::NoGray.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(5).walk(rule)
+      end
+
+      rule.issues.should have(1).item
+    end
+  end
+
+end
