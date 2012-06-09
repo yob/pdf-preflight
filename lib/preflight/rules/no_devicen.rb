e27ed860c1ed3a4fe0c03fe134654a4655f65e7a
James Healy <jimmy@deefa.com>
2012-06-09 16:09:25 +1000

    new rule NoDevicen
    * for detecting DeviceN colour spaces
    * DeviceN colours are mroe or less Separation colour on steroids to
      allow multiple Separations to be mixed

---
+++ lib/preflight/rules/no_devicen.rb
@@ -0,0 +1,94 @@
+# coding: utf-8
+
+require 'forwardable'
+
+module Preflight
+  module Rules
+
+    # Some print workflows forbid the use of DeviceN colours (one or
+    # more spot colours).
+    #
+    # Arguments: none
+    #
+    # Usage:
+    #
+    #   class MyPreflight
+    #     include Preflight::Profile
+    #
+    #     rule Preflight::Rules::NoDevicen
+    #   end
+    #
+    class NoDevicen
+      extend  Forwardable
+
+      # Graphics State Operators
+      def_delegators :@state, :save_graphics_state, :restore_graphics_state
+
+      # Matrix Operators
+      def_delegators :@state, :concatenate_matrix
+
+      attr_reader :issues
+
+      # we're about to start a new page, reset state
+      #
+      def page=(page)
+        @page   = page
+        @state  = PDF::Reader::PageState.new(page)
+        @issues = []
+        @resource_labels_seen = []
+      end
+
+      # descend into nested form xobjects
+      #
+      def invoke_xobject(label)
+        @state.invoke_xobject(label) do |xobj|
+          case xobj
+          when PDF::Reader::FormXObject then
+            xobj.walk(self)
+          end
+        end
+      end
+
+      def set_stroke_color_space(label)
+        check_color_space(label)
+      end
+
+      def set_nonstroke_color_space(label)
+        check_color_space(label)
+      end
+
+      private
+
+      def plain_devicen_names(cs)
+        if cs.is_a?(Array) && cs[0] == :DeviceN
+          @page.objects.deref(cs[1])
+        end
+      end
+
+      def indexed_devicen_names(cs)
+        if cs.is_a?(Array) && cs[0] == :Indexed
+          indexed_base = @page.objects.deref(cs[1])
+          if indexed_base.is_a?(Array) && indexed_base[0] == :DeviceN
+            @page.objects.deref(indexed_base[1])
+          end
+        end
+      end
+
+      def check_color_space(label)
+        return if @resource_labels_seen.include?(label)
+
+        cs           = @state.find_color_space(label)
+        spot_names   = plain_devicen_names(cs)
+        spot_names ||= indexed_devicen_names(cs)
+        if spot_names
+          @issues << Issue.new("DeviceN color detected", self, :page => @page.number,
+                                                               :names => spot_names)
+        end
+
+        @resource_labels_seen << label
+      end
+
+    end
+
+  end
+end
---
+++ spec/rules/no_devicen_spec.rb
@@ -0,0 +1,178 @@
+require File.dirname(__FILE__) + "/../spec_helper"
+
+describe Preflight::Rules::NoDevicen do
+
+  context "a page with a DeviceN fill colour" do
+    let!(:filename) { pdf_spec_file("devicen") }
+
+    it "should have one an issue" do
+      rule     = Preflight::Rules::NoDevicen.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(1).walk(rule)
+      end
+
+      rule.issues.should have(1).item
+    end
+  end
+
+  context "a page with a DeviceN stroke colour" do
+    let!(:filename) { pdf_spec_file("devicen") }
+
+    it "should have one an issue" do
+      rule     = Preflight::Rules::NoDevicen.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(2).walk(rule)
+      end
+
+      rule.issues.should have(1).item
+    end
+
+    it "should return the colourant names in the issue" do
+      rule     = Preflight::Rules::NoDevicen.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(2).walk(rule)
+      end
+
+      issue = rule.issues.first
+      issue.names.should == [:Orange]
+    end
+  end
+
+  context "a page with a Indexed DeviceN fill colour" do
+    let!(:filename) { pdf_spec_file("devicen") }
+
+    it "should have one an issue" do
+      rule     = Preflight::Rules::NoDevicen.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(3).walk(rule)
+      end
+
+      rule.issues.should have(1).item
+    end
+
+    it "should return the colourant names in the issue" do
+      rule     = Preflight::Rules::NoDevicen.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(3).walk(rule)
+      end
+
+      issue = rule.issues.first
+      issue.names.should == [:Orange]
+    end
+  end
+
+  context "a page with a Indexed Separation stroke colour" do
+    let!(:filename) { pdf_spec_file("devicen") }
+
+    it "should have one an issue" do
+      rule     = Preflight::Rules::NoDevicen.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(4).walk(rule)
+      end
+
+      rule.issues.should have(1).item
+    end
+
+    it "should return the colourant names in the issue" do
+      rule     = Preflight::Rules::NoDevicen.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(4).walk(rule)
+      end
+
+      issue = rule.issues.first
+      issue.names.should == [:Orange]
+    end
+  end
+
+  context "a page with an RGB fill colour" do
+    let!(:filename) { pdf_spec_file("rgb") }
+
+    it "should have no issues" do
+      rule     = Preflight::Rules::NoCmyk.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(1).walk(rule)
+      end
+
+      rule.issues.should be_empty
+    end
+  end
+
+  context "a page with an RGB stroke colour" do
+    let!(:filename) { pdf_spec_file("rgb") }
+
+    it "should have no issues" do
+      rule     = Preflight::Rules::NoCmyk.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(2).walk(rule)
+      end
+
+      rule.issues.should be_empty
+    end
+  end
+
+  context "a page with an RGB fill colour defined in the page resources" do
+    let!(:filename) { pdf_spec_file("rgb") }
+
+    it "should have no issues" do
+      rule     = Preflight::Rules::NoCmyk.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(3).walk(rule)
+      end
+
+      rule.issues.should be_empty
+    end
+  end
+
+  context "a page with an RGB stroke colour defined in the page resources" do
+    let!(:filename) { pdf_spec_file("rgb") }
+
+    it "should have no issues" do
+      rule     = Preflight::Rules::NoCmyk.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(4).walk(rule)
+      end
+
+      rule.issues.should be_empty
+    end
+  end
+
+  context "a page with an RGB image" do
+    let!(:filename) { pdf_spec_file("rgb") }
+
+    it "should have no issues" do
+      rule     = Preflight::Rules::NoCmyk.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(5).walk(rule)
+      end
+
+      rule.issues.should be_empty
+    end
+  end
+
+  context "a page with a devicen colour that has an RGB alternate" do
+    let!(:filename) { pdf_spec_file("rgb") }
+
+    it "should have no issues" do
+      rule     = Preflight::Rules::NoCmyk.new
+
+      PDF::Reader.open(filename) do |reader|
+        reader.page(6).walk(rule)
+      end
+
+      rule.issues.should be_empty
+    end
+  end
+
+end
